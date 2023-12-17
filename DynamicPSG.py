4f22e7cd53b034cd8b2e4dca697d7387ba028c11
Kristina <alukinov2009@gmail.com>
2023-12-17 18:41:35 +0100

    Update DynamicPSG.py
    untested, undocumented. for now.
    probably doesn't work

--- DynamicPSG.py
+++ DynamicPSG.py
@@ -4,13 +4,14 @@ class dyndow:
     namelist = []
     sglayout = []
     current = None
+    active = False
+    nooftemps = 0
 
     def __init__(self, layouts):
-        for i in layouts:
-            self.sglayout.append(sg.Column(layouts[i], key="_"+i, visible=False))
-            self.namelist.append(i)
+        self.add(layouts)
 
     def start(self, title = "", start = None):
+        self.active = True
         self.window = sg.Window(title, [self.sglayout], finalize=True)
         if start is None:
             start = self.namelist[0]
@@ -18,9 +19,24 @@ class dyndow:
             self.current = "_"+self.namelist[0]
         self.set(start)
 
+    def add(self, layouts, temp = False):
+        for i in layouts:
+            layout = sg.Column(layouts[i], key="_"+i, visible=False)
+            if self.active:
+                self.win().extend_layout(self.win(), [layout])
+            elif not temp:
+                self.namelist.append(i)
+                self.sglayout.append(layout)
+
     def set(self, layout):
         self.layout().update(visible=False)
-        self.current = "_"+layout
+        if type(layout) is str:
+            self.current = "_"+layout
+        else:
+            layout = {"_"+self.nooftemps: layout[0]}
+            self.nooftemps += 1
+            self.add(layout, temp = True)
+            self.current = "__"+layout[0]
         self.layout().update(visible=True)
         self.win().refresh()
 
@@ -28,6 +44,7 @@ class dyndow:
         return self.win().read()
 
     def close(self):
+        self.active = False
         self.win().close()
 
     def win(self):
